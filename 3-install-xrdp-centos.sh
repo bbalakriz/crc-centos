0d155578b79387265793a8c99a5e0f0bd35122e9
bbalakriz <37283315+bbalakriz@users.noreply.github.com>
2021-02-18 22:32:31 +0800

    Update 3-install-xrdp-centos.sh

--- 3-install-xrdp-centos.sh
+++ 3-install-xrdp-centos.sh
@@ -1,18 +1,17 @@
-# Install Desktop Environment
-sudo yum groupinstall -y "Server with GUI"
+# Install Xfce Desktop Environment
+sudo yum install -y epel-release
+sudo yum install -y Xfce 
 
 # Set a password for the logged in user in the centos on GCP so that it can used for the remote connection
 sudo passwd crwbala
 
 # Install Xrdp
-sudo yum install -y epel-release
 sudo yum install -y xrdp 
 sudo systemctl enable xrdp --now
 sudo systemctl status xrdp
 
-# Configure Firewall
-sudo firewall-cmd --new-zone=xrdp --permanent
-sudo firewall-cmd --zone=xrdp --add-port=3389/tcp --permanent
+# Configure Firewall for Xrdp
+sudo firewall-cmd --add-port=3389/tcp --permanent
 sudo firewall-cmd --reload
 
 
