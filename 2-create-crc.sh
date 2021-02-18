307f2a6e0dd1b661571b089c075925200bcf0457
bbalakriz <37283315+bbalakriz@users.noreply.github.com>
2021-02-18 22:40:31 +0800

    Update 2-create-crc.sh

--- 2-create-crc.sh
+++ 2-create-crc.sh
@@ -1,10 +1,6 @@
 # Update CentOS
 sudo yum update 
 
-# If NetworkManager is missing, do the following, else skip it
-sudo passed root
-su -c 'yum install NetworkManager'
-
 # Download crc and check version
 curl -LO https://mirror.openshift.com/pub/openshift-v4/clients/crc/latest/crc-linux-amd64.tar.xz && tar -Jxvf crc-linux-amd64.tar.xz && mkdir -p ~/bin && export PATH=$PATH:~/bin && mv crc-linux*/crc ~/bin/ && rm -rf crc*
 crc version
