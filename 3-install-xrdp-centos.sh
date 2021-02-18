1cbc845e4d2e86c9134c684339cb2f68be512390
bbalakriz <37283315+bbalakriz@users.noreply.github.com>
2021-02-18 18:27:04 +0800

    Update 3-install-xrdp-centos.sh

--- 3-install-xrdp-centos.sh
+++ 3-install-xrdp-centos.sh
@@ -1,3 +1,10 @@
-Follow the instructions in the artcile[1] to install xrdp on the remote CentOs 8 
+1. Set a password for the logged in user in the centos on GCP so that it can used for the remote connection
+
+2. Follow the instructions in the artcile[1] to install xrdp on the remote CentOs 8 
 
 [1]- https://linuxize.com/post/how-to-install-xrdp-on-centos-8/
+
+
+3. Then use a RDP client like VNC to connect to the server like this. 
+
+
